401549bea78e1ce72a145eb284e2d5782818fb8d
Milan Wilczak <milan.wilczak@24i.com>
2021-07-13 13:36:13 +0200

    Install test runner

---
+++ App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import renderer, { ReactTestRendererJSON } from "react-test-renderer"
+
+import App from "./App"
+
+describe("Application", () => {
+    it("initially shows decks list", () => {
+        fail("I have no idea how to write such test")
+    })
+})
